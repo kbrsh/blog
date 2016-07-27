5de0dea81132aa3c24726e6addca02785b6dea51
kingsofcoding <simplecooldude1@gmail.com>
2016-07-27 21:41:39 +0000

    make more detailed post glance

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var path = __dirname + "/src/posts";
 fs.readdir(path, function(err, files) {
     files.forEach(function(file) {
         var markedFile = marked(fs.readFileSync(__dirname + "/src/posts/" + file, "utf-8"));
-        html.push("<div class='post'><h3>" + markedFile.meta.title + "</h3></div>");
+        html.push("<div class='post'><h3>" + markedFile.meta.title + "</h3><p>" + markedFile.meta.description + "</p><a class='' href='" + file.replace(".md", "") + "'>Read</a></div>");
     });
     
     var newLanding = landingTemplate.replace("{{posts}}", html);
