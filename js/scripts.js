ff2b9468aba210dd4585e166bc46ee92b5c19556
kingsofcoding <simplecooldude1@gmail.com>
2016-07-27 20:49:10 +0000

    random highlight

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,10 @@ document.querySelector('.button_container').addEventListener("click", function()
      document.querySelector('.overlay').classList.toggle('open');
    });
  }
+
+/* Random Highlight */
+var pick=~~(Math.random()*359),
+    tag=document.createElement('style'),
+    style='::-moz-selection {color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::-webkit-selection{color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}::selection{color:white;text-shadow:rgba(0,0,0,.1)1px 2px 2px;background-color:hsl($pick,75%,50%)!important}';
+  tag.innerHTML=style.replace(/\$pick/g,pick);
+  document.body.appendChild(tag);
