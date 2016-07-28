449a75f3354c3f7c6a52982b2e2f945f95983e7b
kingsofcoding <simplecooldude1@gmail.com>
2016-07-28 00:27:03 +0000

    centering in css

--- posts/centering-in-css/js/scripts.js
+++ posts/centering-in-css/js/scripts.js
@@ -2,7 +2,7 @@ var elem = document.getElementById("post").textContent;
 
 /* calculate read time */
 var readTime = function( elem ) {
-  var wpm = 200,                      // words per minute the average person reads
+  var wpm = 175,                      // words per minute the average person reads
       count = elem.split(' ').length; // get word count from specified element
   return Math.ceil( count/wpm );      // return number of minutes
 };
