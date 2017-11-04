967827c1f94dbd390ef5d40f072fec5ac0e031d8
Kabir Shah <iamkbrsh@gmail.com>
2017-11-03 22:58:46 -0700

    add simple math support

--- soldfile.js
+++ soldfile.js
@@ -1,13 +1,80 @@
-const Sold = require('sold');
-const Handlebars = require('handlebars');
+const Sold = require("sold");
+const Handlebars = require("handlebars");
+const Himalaya = require("himalaya");
+const toHTML = require("himalaya/translate").toHTML;
+const minifyHTML = require("html-minifier").minify;
+const MathJax = require("mathjax-node");
+
+MathJax.start();
 
 Handlebars.registerHelper("reverse", function(arr) {
   arr.reverse();
 });
 
+const mathLink = Himalaya.parse(`<link rel="stylesheet" type="text/css" href="../css/post-math.css">`)[0];
+
+const loop = (arr, body, done) => {
+  const length = arr.length;
+  let i = 0;
+
+  const next = () => {
+    if(i === length) {
+      done();
+    } else {
+      body(i++, next);
+    }
+  }
+
+  next();
+}
+
+const compileTemplate = (template, data) => {
+  return minifyHTML(Handlebars.compile(template)(data), {
+    caseSensitive: true,
+    keepClosingSlash: true,
+    removeAttributeQuotes: false,
+    collapseWhitespace: true
+  });
+}
+
 Sold({
   root: __dirname,
   template: "template",
   source: "src",
-  destination: ""
+  destination: "",
+  engine: (template, data, options, done) => {
+    if(data.math === true) {
+      const compiledHTML = Himalaya.parse(data.content);
+      loop(compiledHTML, (i, next) => {
+        const element = compiledHTML[i];
+        if(element.type === "Element" && element.tagName === "pre") {
+          const code = element.children[0];
+          if(code.attributes.className[0] === "lang-math") {
+            MathJax.typeset({
+              math: code.children[0].content,
+              type: "TeX",
+              html: true,
+              css: true
+            }, (data) => {
+              const compiledMath = Himalaya.parse(data.html)[0];
+              compiledMath.attributes.className.push("post-math");
+              compiledHTML[i] = compiledMath;
+              next();
+            });
+          } else {
+            next();
+          }
+        } else {
+          next();
+        }
+      }, () => {
+        data.content = toHTML(compiledHTML);
+        const compiledTemplate = Himalaya.parse(compileTemplate(template, data));
+        compiledTemplate[1].children[0].children.splice(10, 0, mathLink);
+        done(toHTML(compiledTemplate));
+      });
+    } else {
+      done(compileTemplate(template, data));
+    }
+  }
 });
--- src/posts/gradient-descent.md
+++ src/posts/gradient-descent.md
@@ -2,6 +2,7 @@
 title: Gradient Descent
 date: November 1, 2017
 order: 7
+math: true
 ---
 
 Neural networks work by getting a set of inputs, multiplying them by a set of weights, and then returning an output. At first, the weights are initialized with random values. Eventually, the neural network refines the weights so that it can return a plausible output for a given input.
@@ -9,3 +10,11 @@ Neural networks work by getting a set of inputs, multiplying them by a set of we
 A neural network uses a _loss function_ to find how wrong the output was. The goal of the network is to change the weights in such a way that the loss function returns an output close to zero. A low loss value means that the output for all inputs was incredibly close to the expected output for the inputs.
 
 A method called _gradient descent_ is often used to refine ("train") the weights so that the loss value gradually becomes smaller.
+
+### Two Variables
+
+Instead of jumping right into a 500-dimensional gradient descent example, let's start out with a simple function that uses two variables.
+
+```math
+f(x, w) = xw
+```
