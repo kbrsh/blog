3d7e51cfc66be5815ec3ac8e73efc15f58e6fb7b
Kabir Shah <iamkbrsh@gmail.com>
2018-05-07 18:57:34 -0700

    remove js

--- js/comments.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var d = document, s = d.createElement('script');
-    s.src = '//http-blog-kabir-ml.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-})();
