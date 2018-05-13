ea337c966a5035704ef8e79619fbaa9c1c4c1814
Kabir Shah <iamkbrsh@gmail.com>
2018-05-12 23:25:47 -0700

    style keywords

--- soldfile.js
+++ soldfile.js
@@ -28,8 +28,8 @@ MathJax.start();
 Handlebars.registerHelper("list", function(arr) {
   arr.reverse();
   let i = arr.length;
-  while((i--) !== 0) {
-    if(arr[i].draft) {
+  while ((i--) !== 0) {
+    if (arr[i].draft) {
       arr.splice(i, 1);
     }
   }
@@ -39,7 +39,7 @@ const escape = (text) => text.replace(escapeRE, (match) => escapeMap[match]);
 
 const loop = (arr, body, done) => {
   const next = () => {
-    if(arr.length === 0) {
+    if (arr.length === 0) {
       done();
     } else {
       body(arr.shift(), next);
@@ -52,15 +52,15 @@ const loop = (arr, body, done) => {
 const highlight = function(compiled, lang) {
   compiled = compiled.replace(STR_RE, "<span class=\"string\">$1$2$1</span>");
 
-  if(lang === "html") {
+  if (lang === "html") {
     compiled = compiled.replace(HTML_COMMENT_RE, "<span class=\"comment\">$1</span>");
     compiled = compiled.replace(HTML_TAG_RE, function(match, start, content, end) {
-      if(content === undefined) {
+      if (content === undefined) {
         content = '';
       } else {
         content = content.replace(HTML_ATTRIBUTE_RE, function(match, name, value) {
-          if(value !== "string") {
-            if(value === undefined) {
+          if (value !== "string") {
+            if (value === undefined) {
               value = '';
             } else {
               value = '=' + value;
@@ -107,7 +107,7 @@ const typeSet = (math, display, parentChildren, index, next) => {
   }, (data) => {
     const compiledMath = Himalaya.parse(data.html)[0];
 
-    if(display === true) {
+    if (display === true) {
       compiledMath.attributes.className.push("post-math");
     } else {
       const className = compiledMath.attributes.className;
@@ -126,7 +126,7 @@ Sold({
   source: "src",
   destination: '',
   engine: (template, data, options, done) => {
-    if(data.content === undefined) {
+    if (data.content === undefined) {
       done(compileTemplate(template, data));
     } else {
       let compiledHTML = Himalaya.parse(data.content);
@@ -140,21 +140,21 @@ Sold({
 
       loop(elements, (data, next) => {
         const element = data.element;
-        if(element.type === "Element") {
+        if (element.type === "Element") {
           const tagName = element.tagName;
           const children = element.children;
 
-          if(tagName === "pre") {
+          if (tagName === "pre") {
             const code = children[0];
             const codeText = code.children[0].content;
             const codeAttributes = code.attributes;
             const codeAttributesClassName = codeAttributes.className;
 
-            if(codeAttributesClassName === undefined) {
+            if (codeAttributesClassName === undefined) {
               next();
             } else {
               const lang = codeAttributesClassName[0].substring(5);
-              if(lang === "math") {
+              if (lang === "math") {
                 typeSet(codeText, true, data.parentChildren, data.index, next);
               } else {
                 codeAttributes.lang = lang;
@@ -163,16 +163,16 @@ Sold({
                 next();
               }
             }
-          } else if(tagName === "code") {
+          } else if (tagName === "code") {
             const codeText = children[0].content;
-            if(codeText[0] === '$' && codeText[codeText.length - 1] === '$') {
+            if (codeText[0] === '$' && codeText[codeText.length - 1] === '$') {
               typeSet(codeText.slice(1, -1), false, data.parentChildren, data.index, next);
             } else {
               element.children = Himalaya.parse(highlight(codeText, "js"));
               next();
             }
           } else {
-            for(let i = 0; i < children.length; i++) {
+            for (let i = 0; i < children.length; i++) {
               elements.push({
                 parentChildren: children,
                 index: i,
